fa3a1589fbe38cf6d5bbd5715482ec73d049de9b
VennV <111500380+VennDev@users.noreply.github.com>
2024-07-14 18:40:56 +0700

    Add files via upload

--- src/venndev/vform/Main.php
+++ src/venndev/vform/Main.php
@@ -8,12 +8,17 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
 use Throwable;
 use venndev\vform\forms\BreakForm;
 use venndev\vform\forms\JoinForm;
 use venndev\vform\forms\PlaceForm;
+use venndev\vform\forms\SneakingForm;
+use venndev\vformoopapi\attributes\custom\VInput;
+use venndev\vformoopapi\attributes\normal\VButton;
 use vennv\vapm\VapmPMMP;
 
 final class Main extends PluginBase implements Listener
@@ -37,7 +42,7 @@ final class Main extends PluginBase implements Listener
     public function onJoin(PlayerJoinEvent $event): void
     {
         $player = $event->getPlayer();
-        JoinForm::send($player);
+        JoinForm::getInstance($player)->sendForm();
     }
 
     /**
@@ -46,7 +51,7 @@ final class Main extends PluginBase implements Listener
     public function onBreak(BlockBreakEvent $event): void
     {
         $player = $event->getPlayer();
-        BreakForm::send($player);
+        BreakForm::getInstance($player)->sendForm();
     }
 
     /**
@@ -55,7 +60,27 @@ final class Main extends PluginBase implements Listener
     public function onPlace(BlockPlaceEvent $event): void
     {
         $player = $event->getPlayer();
-        PlaceForm::send($player);
+        PlaceForm::getInstance($player)->sendForm();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function onSneak(PlayerToggleSneakEvent $event): void
+    {
+        $player = $event->getPlayer();
+
+        $sneakingForm = SneakingForm::getInstance($player);
+
+        // Add a simple button
+        $sneakingForm->addContent(new VButton(
+            text: "Test Button B",
+            image: "https://example.com/image.png"
+        ), function(Player $player, mixed $data): void {
+            $player->sendMessage("You clicked the test button B!");
+        });
+
+        $sneakingForm->sendForm();
     }
 
 }
